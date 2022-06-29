49f7870fe2d71f48e9999fe732b527210be09557
Austin <austinlasseter@gmail.com>
2022-06-29 15:14:00 -0400

    add favicon

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 import dash
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output, State
-
+import base64
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
@@ -11,10 +11,9 @@ import cv2
 
 ########### Define your variables ######
 
-myheading1='Try out a palindrome here!'
 tabtitle = 'cats vs dogs'
-sourceurl = 'https://www.grammarly.com/blog/16-surprisingly-funny-palindromes/'
-githublink = 'https://github.com/plotly-dash-apps/202-palindrome-callbacks'
+sourceurl = 'https://www.kaggle.com/c/dogs-vs-cats'
+githublink = 'https://github.com/plotly-dash-apps/604-cats-dogs-classifier'
 
 # Load the trained model
 model = load_model('DVC2.h5',compile=True)
@@ -22,17 +21,27 @@ model = load_model('DVC2.h5',compile=True)
 
 ######## Define helper functions
 
-def make_prediction(img_b64):
-    img = image.load_img(img_b64, target_size=(64, 64))
+
+def b64_jpg_converter(base64_string):
+    if 'base64,' in base64_string:
+        img_str=base64_string.split('base64,')[1]
+        imgdata = base64.b64decode(img_str)
+        filename = 'temp.jpg'  # I assume you have a way of picking unique filenames
+        with open(filename, 'wb') as f:
+            f.write(imgdata)
+
+def make_prediction(img_file):
+    img = image.load_img(img_file, target_size=(64, 64))
     img = np.reshape(img,[1,64,64,3])
     img = tf.cast(img, tf.float32)
     img=img/255
-    y_pred = round(model.predict(img),4)
-    classes = (y_pred>0.5).astype("int32")
-    if classes[0][0] == 1:
-        return f'DOG probability: {y_pred}'
-    else:
-        return f'CAT probability: {y_pred}'
+    y_pred = model.predict(img)
+    prediction = (y_pred>0.5).astype("int")
+    classes=['DOG', 'CAT']
+    dog_prob=round(y_pred[0][0].astype("float"),4)
+    cat_prob=round(1-y_pred[0][0].astype("float"),4)
+    # return f"It's a {classes[prediction]}! DOG probability: {dog_prob}, CAT probability: {cat_prob}"
+    return f"DOG probability: {dog_prob}, CAT probability: {cat_prob}"
 
 
 ########### Initiate the app
@@ -45,9 +54,8 @@ app.title=tabtitle
 ########### Set up the layout
 
 app.layout = html.Div([
-    html.H1('This is the header'),
-    html.Img(id='top-image'),
-    html.Div(id='base64-string'),
+    html.H1('Cats vs Dogs!'),
+
     dcc.Upload(
         id='upload-image',
         children=html.Div([
@@ -67,7 +75,6 @@ app.layout = html.Div([
         # DO NOT allow multiple files to be uploaded
         multiple=False
     ),
-    html.Div(id='output-image-upload'),
     html.Button(children='Submit', id='submit-val', n_clicks=0,
                     style={
                     'background-color': 'red',
@@ -77,52 +84,54 @@ app.layout = html.Div([
                     'horizontalAlign': 'center'}
                     ),
     html.Div(id='output-div'),
-    dcc.Store(id='intermediate-value')
+    html.Div(id='output-image-upload'),
+    html.Div(id='base64-string'),
+    dcc.Store(id='intermediate-value'),
+    html.Br(),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A("Data Source", href=sourceurl),
 ])
 
 
 @app.callback(Output('output-image-upload', 'children'),
-              # Output('top-image', 'src'),
               Output('intermediate-value', 'data'),
-              Input('upload-image', 'contents'),
-              )
+              Input('upload-image', 'contents'))
 def update_output(contents):
-    # print(contents)
     if contents is not None:
-        # store_data = contents.to_json(date_format='iso', orient='split')
-        return html.Img(src=contents),  contents
+        b64_jpg_converter(contents)
+        return html.Img(src=contents, style={'height':'20%','width':'20%'}),  contents
     else:
         return None, None
 
 
 @app.callback(Output('base64-string', 'children'),
-              Input('intermediate-value', 'data')
-              )
+              Input('intermediate-value', 'data'))
 def update_output(contents):
-    # print(contents)
     if contents is not None:
-        print('contents type: ',type(contents))
         return str(contents)
     else:
         return 'waiting for inputs'
 
 
 
-
 @app.callback(
     Output(component_id='output-div', component_property='children'),
     Input(component_id='submit-val', component_property='n_clicks'),
-    State('intermediate-value', 'data')
-)
+    State('intermediate-value', 'data'))
 def update_output_div(clicks, input_value):
 
     if clicks==0:
         return "waiting for inputs"
     else:
         if input_value is not None:
-            return make_prediction(input_value)
+            return make_prediction('temp.jpg')
         else:
             return "waiting for image"
+
+
+
+
 
 
 ############ Deploy
