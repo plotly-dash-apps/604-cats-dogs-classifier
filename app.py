59e33e2a83b84591192b53abcd349659c7f7a9fb
Austin <austinlasseter@gmail.com>
2022-06-29 16:31:10 -0400

    tarfile

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import base64
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+import tarfile
 
 
 ########### Define your variables ######
